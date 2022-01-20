b55be616a5df12a6554d2f9613e860c8a429379e
SuperJBGit <68823759+SuperJBGit@users.noreply.github.com>
2022-01-20 16:00:49 -0500

    Added my homework.

---
+++ Jacob Binder Homework 2.R
@@ -0,0 +1,15 @@
+rm(list = ls())
+string <- "Hello world!"
+number <- 1.2
+integer <- 1
+complex_1 <- 1
+complex_2 <- 1
+complex <- complex(real = complex_1, imaginary = complex_2)
+logical <- TRUE
+list <- list(string, number, integer, complex, logical)
+string1 <- list[1] #Love how R lists start from 1
+number1 <- list[2]
+integer1 <- list[3]
+complex1 <- list[4]
+logical1 <- list[5]
+#I made an account and sent it to Dmitri before the 1st class
